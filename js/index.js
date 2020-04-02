48dd0e5159553914d032b2489fa2e8008b3a3a49
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-04-02 13:33:43 -0400

    Next functionality works

--- js/index.js
+++ js/index.js
@@ -1,33 +1,70 @@
-const $show = document.querySelector('.show')
-
+let $show = document.querySelector('.show')
 
 // A function that goes to the start of the show
-const setFirstSlide = () => {
+let setFirstSlide = () => {
+	
+	let $slide = document.querySelector('.slide:first-child');
+	// let $slide = document.querySelector('.slide:nth-child(1)');
+	// let $slide = $show.querySelector(':first-child');
+	// let $slide = $show.firstElementChild
+	// let $slide = document.querySelectorAll('.slide')[0]
+
+	$slide.classList.add('current')
 
 }
 
 // A function that goes to the end of the show
-const setLastSlide = () => {
+let setLastSlide = () => {
 
 }
 
 // Remove ".current" from all ".slide"
-const unsetSlides = () => {
+let unsetSlides = () => {
 
 }
 
 // Previous slide
-const prevSlide = () => {
+let prevSlide = () => {
 
 }
 
 // Next slide
-const nextSlide = () => {
+let nextSlide = () => {
+	// 1. Find the .current element
+	let $curr = document.querySelector('.current')
 
+	// 2. Remove the .current class
+	$curr.classList.remove('current')
+
+	// 3. Go to the next sibling element
+	let $next = $curr.nextElementSibling 
+
+	// 4. Check if a slide exist:
+	//      - if so, add current
+	//      - if not, go to the beginning
+
+	if ($next != null) {
+		// Add the current class
+		$next.classList.add('current')
+	} else {
+		// Go back to the beginning
+		setFirstSlide()
+	}
 }
 
 // When the interface has fully loaded...
-const windowLoaded = () => {
+let windowLoaded = () => {
+	setFirstSlide()
+
+	// EVENT LISTENERS
+	let $nextBtn = document.querySelector('#next')
+	$nextBtn.addEventListener('click', nextSlide)
 
 }
 
+
+
+
+
+// Kick off!
+window.addEventListener('load', windowLoaded)
